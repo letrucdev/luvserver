df662215cb64a926c878f701e9721d9172ecddc4
Trực Lê <lechinhtruc999@gmail.com>
2023-02-21 13:10:27 +0700

    update

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const mysql = require("mysql");
 const fs = require("fs");
 const crypto = require("crypto");
 const jwt = require("jsonwebtoken");
+const path = require("path");
 const app = express();
 
 require("dotenv").config();
 
 app.use(express.urlencoded());
 app.use(express.json());
+app.use("/cdn/images", express.static("public/cdn/images"));
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
@@ -43,13 +45,18 @@ const verifyToken = (req, res, next) => {
   }
 };
 
-const Login = (account, password, res) => {
+const Login = (req, res, next) => {
+  const account = req.body.account;
+  const password = crypto
+    .createHash("sha1")
+    .update(req.body.password)
+    .digest("base64");
   db.query(
     `SELECT * FROM user WHERE account='${account}' AND password='${password}'`,
     (err, result) => {
-      if (err) throw err;
+      if (err) return false;
       if (result.length > 0) {
-        res.status(200).json({
+        req.data = {
           status: 200,
           auth: true,
           token: generateToken({
@@ -59,12 +66,41 @@ const Login = (account, password, res) => {
             email: result[0].email,
             accountType: result[0].account_type,
           }),
-        });
+        };
+        next();
       } else {
         res.status(200).json({ status: 200, auth: false });
       }
     }
   );
+};
+
+const Register = (req, res, next) => {
+  const account = req.body.account;
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = crypto
+    .createHash("sha1")
+    .update(req.body.password)
+    .digest("base64");
+  if (email.trim().match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g)) {
+    db.query(`SELECT * FROM user where account='${account}'`, (err, result) => {
+      if (err) throw err;
+      if (result.length > 0) {
+        res.status(200).json({ status: 200, auth: false });
+      } else {
+        db.query(
+          `INSERT INTO user(username, email, account, password) VALUES ('${username}','${email}','${account}','${password}')`,
+          (err) => {
+            if (err) throw err;
+            next();
+          }
+        );
+      }
+    });
+  } else {
+    res.status(200).json({ status: 200, auth: false });
+  }
 };
 
 db.connect(function (err) {
@@ -76,16 +112,11 @@ app.get("/", (req, res) => {
   res.sendStatus(200);
 });
 
-app.post("/login", (req, res) => {
-  const account = req.body.account;
-  const password = crypto
-    .createHash("sha1")
-    .update(req.body.password)
-    .digest("base64");
-  Login(account, password, res);
+app.post("/api/login", Login, (req, res) => {
+  res.status(200).json(req.data);
 });
 
-app.get("/myInfomation", verifyToken, (req, res) => {
+app.get("/api/myInfomation", verifyToken, (req, res) => {
   res.status(200).json({
     id: req.data.id,
     username: req.data.username,
@@ -94,7 +125,7 @@ app.get("/myInfomation", verifyToken, (req, res) => {
   });
 });
 
-app.get("/getMessage", verifyToken, (req, res) => {
+app.get("/api/getMessage", verifyToken, (req, res) => {
   res.json([
     {
       listmessage: [
@@ -105,28 +136,8 @@ app.get("/getMessage", verifyToken, (req, res) => {
   ]);
 });
 
-app.post("/register", async (req, res) => {
-  const account = req.body.account;
-  const username = req.body.username;
-  const email = req.body.email;
-  const password = crypto
-    .createHash("sha1")
-    .update(req.body.password)
-    .digest("base64");
-  db.query(`SELECT * FROM user where account='${account}'`, (err, result) => {
-    if (err) throw err;
-    if (result.length > 0) {
-      res.status(200).json({ status: 200, auth: false });
-    } else {
-      db.query(
-        `INSERT INTO user(username, email, account, password) VALUES ('${username}','${email}','${account}','${password}')`,
-        (err) => {
-          if (err) throw err;
-          res.status(200).json({ status: 200, auth: true });
-        }
-      );
-    }
-  });
+app.post("/api/register", Register, (req, res) => {
+  res.status(200).json({ status: 200, auth: true });
 });
 
 app.listen(process.env.SERVER_PORT, () => {
